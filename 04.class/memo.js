71c066f27444c2147082643aabcc9b879688fa5b
appsu-mi <natubate870@gmail.com>
2024-01-24 17:51:34 +0900

    プロパティとして保持するのではなく引数として外部から渡す形に戻した

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -7,23 +7,19 @@ import sqlite3 from "sqlite3";
 import MemoDbConnection from "./memo_db_connection.js";
 
 class CliResponse {
-  constructor(memo) {
-    this.memo = memo;
-  }
-
-  showMemoList() {
-    this.memo.find_all(db).then((records) => {
+  showList(memo) {
+    memo.find_all(db).then((records) => {
       records.forEach((record) => {
         console.log(record.content.split("\n")[0]);
       });
     });
   }
 
-  showMemo() {
-    this.#buildSelectQuestion("表示したいメモを選んでください", this.memo).then(
+  show(memo) {
+    this.#buildSelectQuestion("表示したいメモを選んでください", memo).then(
       (question) => {
         Enquirer.prompt(question).then((row) => {
-          this.memo.find(row.id).then((record) => {
+          memo.find(row.id).then((record) => {
             console.log(record.content);
           });
         });
@@ -31,11 +27,11 @@ class CliResponse {
     );
   }
 
-  removeMemo() {
-    this.#buildSelectQuestion("削除したいメモを選んでください", this.memo).then(
+  remove(memo) {
+    this.#buildSelectQuestion("削除したいメモを選んでください", memo).then(
       (question) => {
         Enquirer.prompt(question).then((row) => {
-          this.memo.delete(row.id).then(() => {
+          memo.delete(row.id).then(() => {
             console.log("メモを削除しました");
           });
         });
@@ -43,22 +39,22 @@ class CliResponse {
     );
   }
 
-  addMemo() {
+  add(memo) {
     const rl = readline.createInterface({ input: process.stdin });
     let memos = [];
     rl.on("line", (memoLine) => {
       memos.push(memoLine);
     });
     rl.on("close", () => {
-      this.memo.insert(memos.join("\n")).then(() => {
+      memo.insert(memos.join("\n")).then(() => {
         console.log("メモを追加しました");
       });
     });
   }
 
-  #buildSelectQuestion(message) {
+  #buildSelectQuestion(message, memo) {
     return new Promise((resolve) => {
-      this.#convertMemosToPrompt().then((formatted_records) => {
+      this.#convertMemosToPrompt(memo).then((formatted_records) => {
         const question = {
           type: "select",
           name: "id",
@@ -71,9 +67,9 @@ class CliResponse {
     });
   }
 
-  #convertMemosToPrompt() {
+  #convertMemosToPrompt(memo) {
     return new Promise((resolve) => {
-      this.memo.find_all().then((records) => {
+      memo.find_all().then((records) => {
         if (records == 0) {
           throw new Error("メモがありません");
         } else {
@@ -93,14 +89,14 @@ const options = minimist(process.argv.slice(2));
 const db = new sqlite3.Database("./memo.sqlite");
 
 const memo = new MemoDbConnection(db);
-const cliResponse = new CliResponse(memo);
+const cliResponse = new CliResponse();
 
 if (options.l) {
-  cliResponse.showMemoList();
+  cliResponse.showList(memo);
 } else if (options.r) {
-  cliResponse.showMemo();
+  cliResponse.show(memo);
 } else if (options.d) {
-  cliResponse.removeMemo();
+  cliResponse.remove(memo);
 } else if (!process.stdin.isTTY) {
-  cliResponse.addMemo();
+  cliResponse.add(memo);
 }
